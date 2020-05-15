a8a7a0bdaf40df956037d63b32f28ef5f4b97fa9
uhyo <uhyo@uhy.ooo>
2020-05-16 02:44:22 +0900

    fix format again

--- src/components/ArticleDate/index.tsx
+++ src/components/ArticleDate/index.tsx
@@ -3,13 +3,19 @@ import { Frontmatter } from "../../types/article"
 
 type Props = Pick<Frontmatter, "updated" | "published">
 
+const format = (d: Date) => {
+  return `${d.getFullYear()}年${d.getMonth() + 1}月${d.getDate()}日`
+}
+
 export const ArticleDate: React.FC<Props> = ({ updated, published }) => {
-  const fmtOption = {
-    // dateStyle: "full",
-  } as any
-  const fmt = new Intl.DateTimeFormat("ja-JP", fmtOption)
-  const pubd = fmt.format(new Date(published))
-  const upd = updated ? fmt.format(new Date(updated)) : undefined
+  // const fmtOption = {
+  //   // dateStyle: "full",
+  // } as any
+  // const fmt = new Intl.DateTimeFormat("ja-JP", fmtOption)
+  // const pubd = fmt.format(new Date(published))
+  // const upd = updated ? fmt.format(new Date(updated)) : undefined
+  const pubd = format(new Date(published))
+  const upd = updated ? format(new Date(updated)) : undefined
   return (
     <>
       {pubd} 公開{upd ? ` / ${upd} 更新` : null}
