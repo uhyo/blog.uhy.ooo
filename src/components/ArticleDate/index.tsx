410f05ba2f9d56b417a1bf604859e7f94daa0f20
uhyo <uhyo@uhy.ooo>
2020-05-16 02:40:49 +0900

    disable format

--- src/components/ArticleDate/index.tsx
+++ src/components/ArticleDate/index.tsx
@@ -5,8 +5,8 @@ type Props = Pick<Frontmatter, "updated" | "published">
 
 export const ArticleDate: React.FC<Props> = ({ updated, published }) => {
   const fmtOption = {
-    dateStyle: "full",
-  }
+    // dateStyle: "full",
+  } as any
   const fmt = new Intl.DateTimeFormat("ja-JP", fmtOption)
   const pubd = fmt.format(new Date(published))
   const upd = updated ? fmt.format(new Date(updated)) : undefined
